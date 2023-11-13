84599e01fa8c559e138f0fad4bfa305af281a0ac
Mbwika-07 <allanmbwiks@gmail.com>
2023-11-13 21:32:01 +0300

    Personal website

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Get the button element by its id
+const button = document.getElementById("myButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Display a message when the button is clicked
+    alert("Kindly send the message to the following email address : \n\nallanmbwiks@gmail.com. \n\nThank you for your time.");
+});
